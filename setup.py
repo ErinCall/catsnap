1b4e7b3d082a74bd1cb922eb484ede38124ac4af
Andrew Lorente <andrew.lorente@gmail.com>
2012-07-07 13:02:31 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name="catsnap",
+      version="0.0.1",
+      description="catalog and store funny pictures",
+      author="Andrew Lorente",
+      author_email="andrew.lorente@gmail.com",
+      install_requires=[
+          "boto==2.5.2",
+          ],
+      )
