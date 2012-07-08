7c9c17c632557f25092c7a2dcc358a603bfc985f
Andrew Lorente <andrew.lorente@gmail.com>
2012-07-07 19:07:09 -0700

    Move config into a class in __init__

---
+++ catsnap/__init__.py
@@ -0,0 +1,78 @@
+from __future__ import unicode_literals
+import os
+import sys
+import getpass
+import boto
+import ConfigParser
+
+from catsnap import settings
+
+class Config():
+
+    CREDENTIALS_FILE = os.path.join(os.environ['HOME'], '.boto')
+    CONFIG_FILE = os.path.join(os.environ['HOME'], '.catsnap')
+    parser = None
+
+    def ensure_config_files_exist(self):
+        if not all(map(os.path.exists, [ self.CONFIG_FILE,
+                                         self.CREDENTIALS_FILE ])):
+            sys.stdout.write("Looks like this is your first run.\n")
+            if not os.path.exists(self.CONFIG_FILE):
+                config = self.get_catsnap_config()
+                with open(self.CONFIG_FILE, 'w', 0600) as config_file:
+                    config_file.write(config)
+            if not os.path.exists(self.CREDENTIALS_FILE):
+                credentials = self.get_aws_credentials()
+                with open(self.CREDENTIALS_FILE, 'w', 0600) as creds_file:
+                    creds_file.write(credentials)
+        else:
+            return
+
+    def get_aws_credentials(self):
+        sys.stdout.write("Find your credentials at https://portal.aws.amazon.com/"
+                         "gp/aws/securityCredentials\n")
+        key_id = getpass.getpass('Enter your access key id: ')
+        key = getpass.getpass('Enter your secret access key: ')
+        return """[Credentials]
+aws_access_key_id = %(key_id)s
+aws_secret_access_key = %(key)s""" % {'key_id': key_id, 'key': key}
+
+    def get_catsnap_config(self):
+        bucket_name = '%s-%s' % (settings.BUCKET_BASE, os.environ['USER'])
+        actual_bucket_name = self._input("Please name your bucket (leave blank to "
+                "use '%s'): " % bucket_name)
+        bucket_name = actual_bucket_name or bucket_name
+
+        table_name = bucket_name
+        actual_table_name = self._input("Please name your table (leave blank to "
+                "use '%s'): " % table_name)
+        table_name = actual_table_name or table_name
+        return """[catsnap]
+bucket = %s
+table = %s""" % (bucket_name, table_name)
+
+    def bucket(self):
+        bucket_name = self._bucket_name()
+        s3 = boto.connect_s3()
+        all_buckets = [x.name for x in s3.get_all_buckets()]
+        if bucket_name not in all_buckets:
+            bucket = s3.create_bucket(bucket_name)
+        else:
+            bucket = s3.get_bucket(bucket_name)
+        return bucket
+
+    def _bucket_name(self):
+        return self._parser().get('catsnap', 'bucket')
+
+    def _table_name(self):
+        return self._parser().get('catsnap', 'table')
+
+    def _parser(self):
+        if self.parser is None:
+            parser = ConfigParser.ConfigParser()
+            parser.read(self.CONFIG_FILE)
+            self.parser = parser
+        return self.parser
+
+    def _input(self, *args, **kwargs):
+        return raw_input(*args, **kwargs)
--- tests/test_config.py
+++ tests/test_config.py
@@ -6,21 +6,21 @@ from mock import patch, call, Mock
 from nose.tools import eq_
 
 from catsnap import settings
-from catsnap import config
+from catsnap import Config
 
 class TestConfig():
 
-    @patch('catsnap.config.sys')
-    @patch('catsnap.config.get_aws_credentials')
-    @patch('catsnap.config.get_catsnap_config')
+    @patch('catsnap.sys')
+    @patch('catsnap.Config.get_aws_credentials')
+    @patch('catsnap.Config.get_catsnap_config')
     def test_ensure_config_files_exist__degenerate_case(self, setup_boto,
             setup_catsnap, sys):
         (_, creds) = tempfile.mkstemp()
 
-        with patch('catsnap.config.os.path') as path:
+        with patch('catsnap.os.path') as path:
             path.exists.return_value = True
-            with patch('catsnap.config.CREDENTIALS_FILE', creds) as _:
-                config.ensure_config_files_exist()
+            with patch('catsnap.Config.CREDENTIALS_FILE', creds) as _:
+                Config().ensure_config_files_exist()
         eq_(setup_boto.call_count, 0, "get_aws_credentials shouldn't've "
                 "been called")
         eq_(setup_catsnap.call_count, 0, "get_catsnap_config shouldn't've "
@@ -29,60 +29,60 @@ class TestConfig():
                 "been called")
 
     #using @patch(os.path) here will F up the mkstemp call :(
-    @patch('catsnap.config.sys')
-    @patch('catsnap.config.get_catsnap_config')
-    @patch('catsnap.config.get_aws_credentials')
+    @patch('catsnap.sys')
+    @patch('catsnap.Config.get_catsnap_config')
+    @patch('catsnap.Config.get_aws_credentials')
     def test_ensure_aws_config_exists(self, get_creds, get_config, sys):
         get_config.side_effect = AssertionError("shouldn't've been called")
         get_creds.return_value = 'the credentials'
         (_, creds) = tempfile.mkstemp()
 
-        with patch('catsnap.config.os.path') as path:
+        with patch('catsnap.os.path') as path:
             path.exists.side_effect = [ True, False, True, False ]
-            with patch('catsnap.config.CREDENTIALS_FILE', creds) as _:
-                config.ensure_config_files_exist()
+            with patch('catsnap.Config.CREDENTIALS_FILE', creds) as _:
+                Config().ensure_config_files_exist()
         with open(creds, 'r') as creds_file:
             eq_(creds_file.read(), 'the credentials')
         sys.stdout.write.assert_called_with(
                 "Looks like this is your first run.\n")
 
     #using @patch(os.path) here will F up the mkstemp call :(
-    @patch('catsnap.config.sys')
-    @patch('catsnap.config.get_aws_credentials')
-    @patch('catsnap.config.get_catsnap_config')
+    @patch('catsnap.sys')
+    @patch('catsnap.Config.get_aws_credentials')
+    @patch('catsnap.Config.get_catsnap_config')
     def test_ensure_catsnap_config_exists(self, get_config, get_creds, sys):
         get_creds.side_effect = AssertionError("shouldn't've been called")
-        get_config.return_value = 'the config'
+        get_config.return_value = 'the Config'
         (_, conf) = tempfile.mkstemp()
 
-        with patch('catsnap.config.os.path') as path:
+        with patch('catsnap.os.path') as path:
             path.exists.side_effect = [ False, True, False, True ]
-            with patch('catsnap.config.CONFIG_FILE', conf) as _:
-                config.ensure_config_files_exist()
+            with patch('catsnap.Config.CONFIG_FILE', conf) as _:
+                Config().ensure_config_files_exist()
         with open(conf, 'r') as config_file:
-            eq_(config_file.read(), 'the config')
+            eq_(config_file.read(), 'the Config')
         sys.stdout.write.assert_called_with(
                 "Looks like this is your first run.\n")
 
-    @patch('catsnap.config.getpass')
-    @patch('catsnap.config.sys')
+    @patch('catsnap.getpass')
+    @patch('catsnap.sys')
     def test_get_credentials(self, sys, getpass):
         getpass.getpass.side_effect = ['access key id', 'secret access key']
 
-        creds = config.get_aws_credentials()
+        creds = Config().get_aws_credentials()
         sys.stdout.write.assert_called_with("Find your credentials at "
                 "https://portal.aws.amazon.com/gp/aws/securityCredentials\n")
         eq_(creds, """[Credentials]
 aws_access_key_id = access key id
 aws_secret_access_key = secret access key""")
 
-    @patch('catsnap.config.os')
-    @patch('catsnap.config._input')
+    @patch('catsnap.os')
+    @patch('catsnap.Config._input')
     def test_get_catsnap_config(self, _input, os):
         os.environ.__getitem__.return_value = 'mcgee'
         _input.return_value = ''
 
-        conf = config.get_catsnap_config()
+        conf = Config().get_catsnap_config()
         _input.assert_has_calls([
             call("Please name your bucket (leave blank to use "
                 "'catsnap-mcgee'): "),
@@ -93,26 +93,26 @@ aws_secret_access_key = secret access key""")
 bucket = catsnap-mcgee
 table = catsnap-mcgee""")
 
-    @patch('catsnap.config.os')
-    @patch('catsnap.config._input')
+    @patch('catsnap.os')
+    @patch('catsnap.Config._input')
     def test_get_catsnap_config__custom_names(self, _input, os):
         os.environ.__getitem__.return_value = 'mcgee'
         _input.side_effect = ['booya', '']
 
-        conf = config.get_catsnap_config()
+        conf = Config().get_catsnap_config()
         eq_(conf, """[catsnap]
 bucket = booya
 table = booya""")
 
         _input.side_effect = ['rutabaga', 'wootabaga']
-        conf = config.get_catsnap_config()
+        conf = Config().get_catsnap_config()
         eq_(conf, """[catsnap]
 bucket = rutabaga
 table = wootabaga""")
 
 class TestGetBucket():
-    @patch('catsnap.config._bucket_name')
-    @patch('catsnap.config.boto')
+    @patch('catsnap.Config._bucket_name')
+    @patch('catsnap.boto')
     def test_does_not_re_create_buckets(self, mock_boto, _bucket_name):
         _bucket_name.return_value = 'oodles'
         mock_bucket = Mock()
@@ -122,12 +122,12 @@ class TestGetBucket():
         s3.get_bucket.return_value = mock_bucket
         mock_boto.connect_s3.return_value = s3
 
-        bucket = config.bucket()
+        bucket = Config().bucket()
         eq_(s3.create_bucket.call_count, 0, "shouldn't've created a bucket")
         eq_(bucket, mock_bucket)
 
-    @patch('catsnap.config._bucket_name')
-    @patch('catsnap.config.boto')
+    @patch('catsnap.Config._bucket_name')
+    @patch('catsnap.boto')
     def test_creates_bucket_if_necessary(self, mock_boto, _bucket_name):
         _bucket_name.return_value = 'galvanized'
         s3 = Mock()
@@ -136,7 +136,7 @@ class TestGetBucket():
         s3.get_all_buckets.return_value = []
         mock_boto.connect_s3.return_value = s3
 
-        bucket = config.bucket()
+        bucket = Config().bucket()
         s3.create_bucket.assert_called_with('galvanized')
         eq_(bucket, mock_bucket)
 
@@ -144,8 +144,9 @@ class TestGetBucket():
         (_, conf) = tempfile.mkstemp()
         with open(conf, 'w') as config_file:
             config_file.write("""[catsnap]
-bucket = boogles""")
+bucket = boogles
+table = boogles""")
 
-        with patch('catsnap.config.CONFIG_FILE', conf) as _:
-            bucket_name = config._bucket_name()
+        with patch('catsnap.Config.CONFIG_FILE', conf) as _:
+            bucket_name = Config()._bucket_name()
         eq_(bucket_name, 'boogles')
