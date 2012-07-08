7ae870fe2abde7b1f73b4f18c3a639a504c39848
Andrew Lorente <andrew.lorente@gmail.com>
2012-07-08 00:58:30 -0700

    Don't overwrite tags, update them!

--- catsnap/tag.py
+++ catsnap/tag.py
@@ -1,9 +1,16 @@
 from __future__ import unicode_literals
 
+from boto.dynamodb.exceptions import DynamoDBKeyNotFoundError
+
 class Tag():
     def __init__(self, name):
         self.name = name
 
     def save(self, table, filename):
-        item = table.new_item(hash_key=self.name, attrs={filename: filename})
+        try:
+            item = table.get_item(self.name)
+            item[filename] = filename
+        except DynamoDBKeyNotFoundError:
+            item = table.new_item(hash_key=self.name,
+                    attrs={filename: filename})
         item.put()
--- tests/test_tags.py
+++ tests/test_tags.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 
-from mock import patch, Mock, call
+from mock import patch, Mock, call, MagicMock
 from nose.tools import eq_, raises
+from boto.dynamodb.exceptions import DynamoDBKeyNotFoundError
 
 from catsnap.tag import Tag
 
@@ -10,9 +11,22 @@ class TestTags():
         table = Mock()
         item = Mock()
         tag = Tag('cat')
+        table.get_item.side_effect = DynamoDBKeyNotFoundError('no such tag')
         table.new_item.return_value = item
 
-        tag.save(table,  'Sewing_cat.gif')
+        tag.save(table, 'Sewing_cat.gif')
         table.new_item.assert_called_with(hash_key='cat',
                 attrs={ 'Sewing_cat.gif': 'Sewing_cat.gif'})
         item.put.assert_called_with()
+
+    def test_save__updates_existing_tag(self):
+        table = Mock()
+        item = MagicMock()
+        table.get_item.return_value = item
+
+        tag = Tag('cat')
+        tag.save(table, 'Other_cat.gif')
+
+        eq_(table.new_item.call_count, 0, "shouldn't've made a new entry")
+        item.put.assert_called_with()
+        item.__setitem__.assert_called_with('Other_cat.gif', 'Other_cat.gif')
