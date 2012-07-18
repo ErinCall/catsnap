03a09f40200a32443511067e11c83843cbf0104d
Andrew Lorente <andrew.lorente@gmail.com>
2012-07-18 16:30:44 -0700

    Allow batch-getting unprocessed tag Items directly

--- catsnap/batch/tag_batch.py
+++ catsnap/batch/tag_batch.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals
 
 from catsnap import Config, HASH_KEY
-from boto.dynamodb.batch import BatchList
+from boto.dynamodb.batch import BatchWriteList, BatchList
+from boto.dynamodb.item import Item
 import json
 
-def get_tags(tag_names):
+def get_tag_items(tag_names):
     if not tag_names:
         raise StopIteration
+
     dynamo = Config().get_dynamodb()
     table = Config().table('tag')
     batch_list = BatchList(dynamo)
@@ -14,9 +16,6 @@ def get_tags(tag_names):
             attributes_to_get=['filenames', HASH_KEY])
     response = dynamo.batch_get_item(batch_list)
     items = response['Responses'][table.name]['Items']
-    for item in items:
-        item['tag'] = item.pop(HASH_KEY)
-        item['filenames'] = json.loads(item['filenames'])
     unprocessed_keys = []
     if response['UnprocessedKeys'] \
             and table.name in response['UnprocessedKeys']:
@@ -25,7 +24,11 @@ def get_tags(tag_names):
 
     for item in items:
         yield item
-    if not unprocessed_keys:
-        raise StopIteration
-    for item in get_tags(unprocessed_keys):
+    for item in get_tag_items(unprocessed_keys):
         yield item
+
+def get_tags(tag_names):
+    for item in get_tag_items(tag_names):
+        yield { 'tag': item[HASH_KEY],
+                'filenames': json.loads(item['filenames'])}
+
