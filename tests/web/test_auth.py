804caef1c0849599712972bc298f7a7a8cb20f20
Andrew Lorente <hello@andrewlorente.com>
2014-10-20 12:20:11 -0700

    Don't use post in the auth tests
    At the time they were written, you could GET /add without auth. Using
    POST /add coupled the auth tests to the internals of the add logic,
    though, so now that GET /add requires auth, we can decouple it!

--- tests/web/test_auth.py
+++ tests/web/test_auth.py
@@ -2,43 +2,30 @@ from __future__ import unicode_literals
 
 import datetime
 import sha
-from mock import patch, Mock
 from tests import TestCase, with_settings
 from nose.tools import eq_
 from catsnap import Client
 
 class TestAuth(TestCase):
     @with_settings(api_key='supersekrit')
-    @patch('catsnap.web.controllers.image.ImageTruck')
-    def test_hmac_auth(self, ImageTruck):
-        truck = Mock()
-        ImageTruck.new_from_url.return_value = truck
-        truck.filename = 'CA7'
-        truck.content_type = "image/jpeg"
-        truck.url.return_value = 'ess three'
-        truck.contents = b''
+    def test_hmac_auth(self):
         now = str(datetime.datetime.utcnow())
         string_to_sign = "%s\n%s" % (now, Client().config().api_key)
         signature = sha.sha(string_to_sign).hexdigest()
 
-        response = self.app.post('/add',
+        response = self.app.get('/add',
                 headers=[
                     ('X-Catsnap-Signature', signature),
-                    ('X-Catsnap-Signature-Date', now)],
-                data={'tags': 'pet cool',
-                    'album': '',
-                    'url': 'http://imgur.com/cat.gif'},
-                follow_redirects=True)
+                    ('X-Catsnap-Signature-Date', now)])
         eq_(response.status_code, 200, response.data)
 
     @with_settings(api_key='supersekrit')
     def test_hmac_auth__fails_if_signature_does_not_match(self):
         now = str(datetime.datetime.utcnow())
-        response = self.app.post('/add',
+        response = self.app.get('/add',
                 headers=[
                     ('X-Catsnap-Signature', 'MALISHUS'),
-                    ('X-Catsnap-Signature-Date', now)],
-                data={'tags': 'pet cool', 'url': 'http://imgur.com/cat.gif'})
+                    ('X-Catsnap-Signature-Date', now)])
         eq_(response.status_code, 302, response.data)
         eq_(response.headers['Location'], 'http://localhost/')
 
@@ -48,10 +35,9 @@ class TestAuth(TestCase):
         string_to_sign = "%s\n%s" % (then, Client().config().api_key)
         signature = sha.sha(string_to_sign).hexdigest()
 
-        response = self.app.post('/add',
+        response = self.app.get('/add',
                 headers=[
                     ('X-Catsnap-Signature', signature),
-                    ('X-Catsnap-Signature-Date', then)],
-                data={'tags': 'pet cool', 'url': 'http://imgur.com/cat.gif'})
+                    ('X-Catsnap-Signature-Date', then)])
         eq_(response.status_code, 302, response.data)
         eq_(response.headers['Location'], 'http://localhost/')
