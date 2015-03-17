69ba557dab80be50ea77d78f2bf0459309d3c830
Andrew Lorente <hello@andrewlorente.com>
2015-03-17 09:28:36 -0700

    Better organization of test fixtures

--- tests/web/test_formatted_routes.py
+++ tests/web/test_formatted_routes.py
@@ -16,6 +16,20 @@ class TestFormattedRoute(TestCase):
             raise AssertionError("Received an unknown format: '%s'" %
                                  request_format)
 
+    @formatted_route('/test/lazy/json/route')
+    def json_action_that_returns_an_object(request_format):
+        if request_format == 'json':
+            return {'current status': 'lazy'}
+        else:
+            raise NotImplementedError
+
+    @formatted_route('/test/fancy/formatted/route',
+                     methods=['GET'],
+                     defaults={'lizard': 'charmander'})
+    @formatted_route('/test/fancy/formatted/route/<lizard>')
+    def fancy_action(request_format, lizard):
+        return json.dumps({'request_format': request_format, 'lizard': lizard})
+
     def test_defaults_to_html_format(self):
         response = self.app.get('/test/formatted/route')
         eq_(response.data, 'HTML')
@@ -41,13 +55,6 @@ class TestFormattedRoute(TestCase):
                                 headers={'Accept':'application/json'})
         eq_(response.data, 'JSON')
 
-    @formatted_route('/test/fancy/formatted/route',
-                     methods=['GET'],
-                     defaults={'lizard': 'charmander'})
-    @formatted_route('/test/fancy/formatted/route/<lizard>')
-    def fancy_action(request_format, lizard):
-        return json.dumps({'request_format': request_format, 'lizard': lizard})
-
     def test_get_fancy_action__routed_default(self):
         response = self.app.get('/test/fancy/formatted/route')
         body = json.loads(response.data)
@@ -57,13 +64,6 @@ class TestFormattedRoute(TestCase):
         response = self.app.get('/test/fancy/formatted/route/komodo.json')
         body = json.loads(response.data)
         eq_(body, {'request_format': 'json', 'lizard': 'komodo'})
-
-    @formatted_route('/test/lazy/json/route')
-    def json_action_that_returns_an_object(request_format):
-        if request_format == 'json':
-            return {'current status': 'lazy'}
-        else:
-            raise NotImplementedError
 
     def test_json_actions_can_return_objects(self):
         response = self.app.get('/test/lazy/json/route.json')
